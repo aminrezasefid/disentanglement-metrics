9a0f686cbea4349d76206e6cd23a07b24243e960
Amin <aminreza_sefid@yahoo.com>
2023-10-23 11:54:19 +0330

    bugsfixed

---
+++ src/calculator.py
@@ -0,0 +1,21 @@
+import metrics
+from metrics.modularity import modularity
+from metrics.z_diff import z_diff
+from metrics.mig_sup import mig_sup
+from metrics.dci import dci
+#,z_diff,mig_sup,dci
+import argparse
+import numpy as np
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('--features', help='features path')
+parser.add_argument('--labels', help='labels path')
+args = parser.parse_args()
+feat_path=args.features
+lbl_path=args.labels
+feat_tensor=np.load(feat_path)
+lbl_tensor=np.load(lbl_path)
+modularity_score=modularity(lbl_tensor,feat_tensor)
+z_diff_score=metrics.z_diff(lbl_tensor,feat_tensor)
+mig_sup_score=metrics.mig_sup(lbl_tensor,feat_tensor)
+dci_score=metrics.dci(lbl_tensor,feat_tensor)
+print(modularity_score,z_diff_score,mig_sup_score,dci_score)
