6883355ec2954f252b9834c29b49f7560a899af2
Amin <aminreza_sefid@yahoo.com>
2023-10-25 19:25:23 +0330

    asdasd

--- src/calculator.py
+++ src/calculator.py
@@ -9,17 +9,17 @@ import numpy as np
 parser = argparse.ArgumentParser(description='Process some integers.')
 parser.add_argument('--features', help='features path')
 parser.add_argument('--labels', help='labels path')
+parser.add_argument('--out',help='labels path')
 args = parser.parse_args()
 feat_path=args.features
 lbl_path=args.labels
+out_path=args.out
 feat_tensor=np.load(feat_path)
 lbl_tensor=np.load(lbl_path)
 modularity_score=modularity(lbl_tensor,feat_tensor)
 #z_diff_score=z_diff(lbl_tensor,feat_tensor)
 #mig_sup_score=mig_sup(lbl_tensor,feat_tensor)
 #dci_score=dci(lbl_tensor,feat_tensor)
-model_name=feat_path.split("_")[1]
-dataset_name=feat_path.split("_")[2].split(".")[0]
-with open(f"metric_{model_name}_{dataset_name}.txt","w") as f:
-    f.write(modularity_score)
+with open(out_path,"w") as f:
+    f.write(str(modularity_score))
 print(modularity_score)
